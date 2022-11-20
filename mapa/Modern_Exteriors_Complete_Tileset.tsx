bfb497c64bcdb55f70d3ecdd27f17513bab75a3c
Konrad Kreczko <konradkreczko@gmail.com>
2022-11-20 15:24:52 +0100

    cos

---
+++ mapa/Modern_Exteriors_Complete_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Modern_Exteriors_Complete_Tileset" tilewidth="16" tileheight="16" tilecount="31856" columns="176">
+ <image source="../../Modern_Exteriors_Complete_Tileset.png" width="2816" height="2896"/>
+</tileset>
